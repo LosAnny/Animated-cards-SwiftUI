667d20859ef866c432c4e04cf1885837ea052748
Anna Loshakova <ann.losh99@mail.ru>
2022-03-22 18:30:14 +0300

    Initial Commit

---
+++ Animated cards SwiftUI/Animated_cards_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animated_cards_SwiftUIApp.swift
+//  Animated cards SwiftUI
+//
+//  Created by Анна Лошакова on 22.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct Animated_cards_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animated cards SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animated cards SwiftUI
+//
+//  Created by Анна Лошакова on 22.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
