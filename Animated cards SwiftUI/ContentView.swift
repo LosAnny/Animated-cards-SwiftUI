580ff8ec26ba3e2431ce776143f202d7b8df63a0
Anna Loshakova <ann.losh99@mail.ru>
2022-03-22 19:09:38 +0300

    Add gestures.

--- Animated cards SwiftUI/ContentView.swift
+++ Animated cards SwiftUI/ContentView.swift
@@ -8,7 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @GestureState var dragState = DragState.inactive
+    
     var body: some View {
+        
+        // Инициализируем жест и обновляем dragState
+        let dragGester = DragGesture()
+           .updating($dragState) { (value, state, transaction) in
+              state = .dragging(translation: value.translation)
+           }
+        
         ZStack {
             Card(title: "Third card")
                 .blendMode(.hardLight)
@@ -19,8 +29,37 @@ struct ContentView: View {
                 .padding(32)
                 .padding(.bottom, 32)
             MainCard(title: "Main card")
+                .offset(
+                      x: dragState.translation.width,
+                      y: dragState.translation.height
+                   )
+                .gesture(dragGester)
         }
     }
+}
+
+enum DragState {
+
+        case inactive
+        case dragging(translation: CGSize)
+
+        var translation: CGSize {
+            switch self {
+            case .inactive:
+                return .zero
+            case .dragging(let translation):
+                return translation
+            }
+        }
+
+        var isActive: Bool {
+            switch self {
+            case .inactive:
+                return false
+            case .dragging:
+                return true
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
